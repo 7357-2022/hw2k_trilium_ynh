77039146d04b8b07e365e9be505f5fd03dc972fd
HW2K Forks <110225308+7357-2022@users.noreply.github.com>
2022-10-10 10:37:12 +0200

    Add files via upload

---
+++ scripts/_common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+nodejs_version=16
+
+# dependencies used by the app
+pkg_dependencies="libpng16-16 libpng-dev pkg-config autoconf libtool build-essential nasm libx11-dev libxkbfile-dev"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
